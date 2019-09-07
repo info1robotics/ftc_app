932ef8ab891926adf2bceb44dd190e33af839ed6
Săndulescu Alexandru <alexandru.prahova@gmail.com>
2019-09-07 19:51:23 +0300

    Added Hooks (arm "fingers")

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm/ArmMotors.java
@@ -0,0 +1,121 @@
+package org.firstinspires.ftc.teamcode.arm;
+
+import android.os.SystemClock;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.teleop.TeleOpBasic;
+
+public class ArmMotors {
+    private DcMotor baseMotorLeft, baseMotorRight;
+    private DcMotor extenderMotor;
+    private DcMotor hookMotorRotator;
+
+    private Servo hookLeft, hookRight;
+
+    private Telemetry console;
+
+    private TeleOpBasic opMode;
+
+    private final double BASE_MOTORS_SPEED = 0.7;
+    private final double EXTENDER_MOTOR_SPEED = 0.6;
+    private final double HOOK_MOTOR_ROTATOR_SPEED = 0.6;
+
+    private final double HOOK_LEFT_POSITION_HOLD = 0.85;
+    private final double HOOK_LEFT_POSITION_MIDDLE = 0.9;
+    private final double HOOK_LEFT_POSITION_IDLE = 1.0;
+    private final double HOOK_RIGHT_POSITION_HOLD = 0.75;
+    private final double HOOK_RIGHT_POSITION_MIDDLE = 0.7;
+    private final double HOOK_RIGHT_POSITION_IDLE = 0.60;
+
+    public ArmMotors(HardwareMap hardwareMap, Telemetry telemetry, TeleOpBasic opMode) {
+        baseMotorLeft = hardwareMap.get(DcMotor.class,
+                "baseMotorLeft");
+        baseMotorRight = hardwareMap.get(DcMotor.class,
+                "baseMotorRight");
+        extenderMotor = hardwareMap.get(DcMotor.class,
+                "extenderMotor");
+        hookMotorRotator = hardwareMap.get(DcMotor.class,
+                "hookMotorRotator");
+
+
+        this.opMode = opMode;
+
+        hookLeft = hardwareMap.get(Servo.class, "hookLeft");
+        hookRight = hardwareMap.get(Servo.class, "hookRight");
+        hookLeft.setPosition(HOOK_LEFT_POSITION_IDLE);
+        hookRight.setPosition(HOOK_RIGHT_POSITION_IDLE);
+
+        hookMotorRotator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        baseMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        baseMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        console = telemetry;
+        console.addData("ArmMotors class", "Set up.");
+    }
+
+    public void raiseArm(double powerFactor) {
+        baseMotorLeft.setPower(powerFactor * BASE_MOTORS_SPEED);
+        baseMotorRight.setPower(-1.0 * powerFactor * BASE_MOTORS_SPEED);
+
+        console.addLine("Raising arm");
+        console.update();
+    }
+
+    public void lowerArm(double powerFactor) {
+        baseMotorLeft.setPower(-1.0 * powerFactor * BASE_MOTORS_SPEED);
+        baseMotorRight.setPower(powerFactor * BASE_MOTORS_SPEED);
+
+        console.addLine("Lowering arm");
+        console.update();
+    }
+
+    public void extendArm(double powerFactor) {
+        extenderMotor.setPower(powerFactor * EXTENDER_MOTOR_SPEED);
+    }
+
+    public void contractArm(double powerFactor) {
+        extenderMotor.setPower(-1.0 * powerFactor * EXTENDER_MOTOR_SPEED);
+    }
+
+    public void raiseHookRotator(double powerFactor) {
+        hookMotorRotator.setPower(powerFactor * HOOK_MOTOR_ROTATOR_SPEED);
+    }
+
+    public void lowerHookRotator(double powerFactor) {
+        hookMotorRotator.setPower(-1.0 * powerFactor * HOOK_MOTOR_ROTATOR_SPEED);
+    }
+
+    public void makeHookIdle() {
+        hookLeft.setPosition(HOOK_LEFT_POSITION_IDLE);
+        hookRight.setPosition(HOOK_RIGHT_POSITION_IDLE);
+    }
+
+    public void makeHookHold() {
+        hookLeft.setPosition(HOOK_LEFT_POSITION_HOLD);
+        hookRight.setPosition(HOOK_RIGHT_POSITION_HOLD);
+
+        opMode.sleep(1000);
+
+        hookLeft.setPosition(HOOK_LEFT_POSITION_MIDDLE);
+        hookRight.setPosition(HOOK_RIGHT_POSITION_MIDDLE);
+
+        opMode.sleep(200);
+
+        hookLeft.setPosition(HOOK_LEFT_POSITION_HOLD);
+        hookRight.setPosition(HOOK_RIGHT_POSITION_HOLD);
+    }
+
+
+    public void stopAll() {
+        baseMotorLeft.setPower(0);
+        baseMotorRight.setPower(0);
+        extenderMotor.setPower(0);
+        hookMotorRotator.setPower(0);
+
+        console.addLine("Stopped arm completely");
+        console.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpBasic.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.arm.ArmMotors;
+import org.firstinspires.ftc.teamcode.movement.MoveRobotLinear;
+
+/**
+ * Class which listens for controller input and calls the correct
+ * method in the Utilities class to perform the action.
+ */
+
+@TeleOp(name = "Basic Manual Control")
+public class TeleOpBasic extends LinearOpMode {
+
+    MoveRobotLinear movementController;
+    ArmMotors armController;
+    private final double MOVEMENT_SPEED = 1.0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        movementController = new MoveRobotLinear(hardwareMap, telemetry,
+                this);
+
+        armController = new ArmMotors(hardwareMap, telemetry, this);
+
+        telemetry.update();
+
+
+        while (!isStarted() && !isStopRequested()) {
+            telemetry.addData("status",
+                    "waiting for start command");
+            telemetry.update();
+            sleep(200);
+        }
+
+
+        while (opModeIsActive()) {
+
+            // Robot Movement
+            if (gamepad1.dpad_down)
+                Utilities.moveBackward(movementController,
+                        gamepad1, MOVEMENT_SPEED, this);
+            if (gamepad1.dpad_up) Utilities.moveForward(movementController,
+                    gamepad1, MOVEMENT_SPEED, this);
+
+            if (gamepad1.dpad_right)
+                Utilities.moveRight(movementController,
+                        gamepad1, MOVEMENT_SPEED, this);
+            if (gamepad1.dpad_left) Utilities.moveLeft(movementController,
+                    gamepad1, MOVEMENT_SPEED, this);
+
+            if (gamepad1.left_trigger > 0)
+                Utilities.spinLeft(movementController, gamepad1, this);
+            if (gamepad1.right_trigger > 0)
+                Utilities.spinRight(movementController, gamepad1, this);
+
+
+            // Arm Movement
+
+            if(gamepad1.left_stick_y != 0) {
+                Utilities.changeArmElevation(armController, gamepad1,
+                        this);
+            }
+
+            if(gamepad1.left_bumper) {
+                Utilities.contractArm(armController, gamepad1, this);
+            }
+
+            if(gamepad1.right_bumper) {
+                Utilities.extendArm(armController, gamepad1, this);
+            }
+
+            if(gamepad1.right_stick_y != 0) {
+                Utilities.changeHookRotatorElevation(armController, gamepad1,
+                        this);
+            }
+
+            if (gamepad1.a) {
+                armController.makeHookIdle();
+            }
+
+            if (gamepad1.b) {
+                armController.makeHookHold();
+            }
+
+            idle();
+            telemetry.addLine("Connection active");
+            telemetry.update();
+        }
+
+    }
+
+}
